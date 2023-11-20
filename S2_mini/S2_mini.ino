179575377614212f3b4c6734ca5dd41417426347
Robert Kaveh <mehdadoo@live.com>
2023-11-20 03:40:00 +0100

    updates the ST7735 display with squares corresponding to the button presses
    In this code, we've added the `updateDisplay` function, which takes an array of button states as an argument and updates the ST7735 display with squares corresponding to the button presses. The `handleButtonStates` function now also calls `updateDisplay` whenever there is a change in the button states.
    
    The `setup` function initializes the ST7735 display and clears the screen to black. The `loop` function remains the same, continuing to call `handleButtonStates` and including a delay for debouncing.
    
    This code should now display white squares on the ST7735 display whenever the corresponding buttons are pressed, in addition to sending the button states over UART

--- S2_mini/S2_mini.ino
+++ S2_mini/S2_mini.ino
@@ -1,4 +1,6 @@
 #include <HardwareSerial.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
 
 // Use UART1
 HardwareSerial SerialPort(1);
@@ -7,6 +9,16 @@ HardwareSerial SerialPort(1);
 const int SERIAL_TX_PIN = 9;
 const int SERIAL_RX_PIN = 11;
 
+// ST7735 display pins
+#define TFT_CS   21
+#define TFT_RST  34 
+#define TFT_DC   36
+#define TFT_MOSI 38  
+#define TFT_SCLK 40  
+
+// Initialize Adafruit ST7735 with hardware SPI
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
 // Button pin assignments
 const int buttonPins[] = {3, 5, 7, 2, 4, 6};
 const int numButtons = sizeof(buttonPins) / sizeof(buttonPins[0]);
@@ -14,48 +26,60 @@ const int numButtons = sizeof(buttonPins) / sizeof(buttonPins[0]);
 // Variables to track previous button states
 int prevButtonStates[numButtons] = {0};
 
+// Function to update the display with squares corresponding to button states
+void updateDisplay(const int buttonStates[]) {
+  tft.fillScreen(ST7735_BLACK); // Clear the screen
+
+  for (int i = 0; i < numButtons; i++) {
+    if (buttonStates[i]) {
+      // Draw a filled square for each pressed button
+      tft.fillRect(i * 20, 50, 18, 18, ST7735_WHITE);
+    }
+  }
+}
+
 // Function to handle the main logic of checking button states and sending data
-void handleButtonStates() 
-{
+void handleButtonStates() {
   bool stateChanged = false;
   String message = "START";
+  int buttonStates[numButtons] = {0};
 
-  for (int i = 0; i < numButtons; i++) 
-  {
-    int currentButtonState = !digitalRead(buttonPins[i]);
+  for (int i = 0; i < numButtons; i++) {
+    buttonStates[i] = !digitalRead(buttonPins[i]);
     // Check if button state has changed
-    if (prevButtonStates[i] != currentButtonState) 
-    {
-      message += "|B" + String(buttonPins[i]) + ":" + String(currentButtonState);
-      prevButtonStates[i] = currentButtonState;
+    if (prevButtonStates[i] != buttonStates[i]) {
+      message += "|B" + String(buttonPins[i]) + ":" + String(buttonStates[i]);
+      prevButtonStates[i] = buttonStates[i];
       stateChanged = true;
     }
   }
 
-  if (stateChanged) 
-  {
+  if (stateChanged) {
     message += "|END";
     // Print the message to Serial Monitor
     Serial.println(message);
     // Send the message over UART
     SerialPort.print(message);
+    // Update the display
+    updateDisplay(buttonStates);
   }
 }
 
-void setup() 
-{
+void setup() {
   Serial.begin(115200);
   SerialPort.begin(115200, SERIAL_8N1, SERIAL_TX_PIN, SERIAL_RX_PIN);
 
   // Initialize button pins as inputs with internal pull-up
-  for (int i = 0; i < numButtons; i++) 
-  {
+  for (int i = 0; i < numButtons; i++) {
     pinMode(buttonPins[i], INPUT_PULLUP);
   }
+
+  // Initialize the ST7735 display
+  tft.initR(INITR_BLACKTAB); // Init display with black tab
+  tft.fillScreen(ST7735_BLACK); // Clear the screen
 }
 
-void loop() 
-{
+void loop() {
   handleButtonStates(); // Function call for handling button states
   delay(25); // Delay to avoid excessive looping
 }
