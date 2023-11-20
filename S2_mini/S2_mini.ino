52edf55221ff612d71d5020b14223913cc2d9ef6
Robert Kaveh <mehdadoo@live.com>
2023-11-20 03:36:50 +0100

    more robust and cleaner structure
    In this optimized code, we use an array called `buttonPins` to store the pin numbers for the buttons. We also have an array called `prevButtonStates` to keep track of the previous state of each button. The `numButtons` variable calculates the number of buttons based on the size of the `buttonPins` array.
    
    The `handleButtonStates` function now loops through each button, checks its state, and builds the message string if any state changes are detected. If any button state has changed, the message is sent over UART, and the previous button states are updated.

--- S2_mini/S2_mini.ino
+++ S2_mini/S2_mini.ino
@@ -1,104 +1,61 @@
 #include <HardwareSerial.h>
 
 // Use UART1
-HardwareSerial SerialPort(1); 
+HardwareSerial SerialPort(1);
 
 // Pins for UART1
 const int SERIAL_TX_PIN = 9;
 const int SERIAL_RX_PIN = 11;
 
-// Pins for buttons
-const int BUTTON_3_PIN = 3;
-const int BUTTON_5_PIN = 5;
-const int BUTTON_7_PIN = 7;
-const int BUTTON_36_PIN = 36;
-const int BUTTON_38_PIN = 38;
-const int BUTTON_40_PIN = 40;
+// Button pin assignments
+const int buttonPins[] = {3, 5, 7, 2, 4, 6};
+const int numButtons = sizeof(buttonPins) / sizeof(buttonPins[0]);
 
 // Variables to track previous button states
-int prevButton3State = 0;
-int prevButton5State = 0;
-int prevButton7State = 0;
-int prevButton36State = 0;
-int prevButton38State = 0;
-int prevButton40State = 0;
+int prevButtonStates[numButtons] = {0};
 
 // Function to handle the main logic of checking button states and sending data
-void handleButtonStates() {
-  // Read the state of buttons
-  int button3State = !digitalRead(BUTTON_3_PIN);
-  int button5State = !digitalRead(BUTTON_5_PIN);
-  int button7State = !digitalRead(BUTTON_7_PIN);
-  int button36State = !digitalRead(BUTTON_36_PIN);
-  int button38State = !digitalRead(BUTTON_38_PIN);
-  int button40State = !digitalRead(BUTTON_40_PIN);
+void handleButtonStates() 
+{
+  bool stateChanged = false;
+  String message = "START";
 
-  // Check if any button state has changed
-  if (prevButton3State != button3State || prevButton5State != button5State || prevButton7State != button7State || prevButton36State != button36State || prevButton38State != button38State || prevButton40State != button40State) {
-    // Creating the message string
-    String message = "START";
+  for (int i = 0; i < numButtons; i++) 
+  {
+    int currentButtonState = !digitalRead(buttonPins[i]);
+    // Check if button state has changed
+    if (prevButtonStates[i] != currentButtonState) 
+    {
+      message += "|B" + String(buttonPins[i]) + ":" + String(currentButtonState);
+      prevButtonStates[i] = currentButtonState;
+      stateChanged = true;
+    }
+  }
 
-    // Append the changed button states to the message
-    if (prevButton3State != button3State) {
-      message += "|B3:";
-      message += button3State;
-    }
-    if (prevButton5State != button5State) {
-      message += "|B5:";
-      message += button5State;
-    }
-    if (prevButton7State != button7State) {
-      message += "|B7:";
-      message += button7State;
-    }
-    if (prevButton36State != button36State) {
-      message += "|B36:";
-      message += button36State;
-    }
-    if (prevButton38State != button38State) {
-      message += "|B38:";
-      message += button38State;
-    }
-    if (prevButton40State != button40State) {
-      message += "|B40:";
-      message += button40State;
-    }
-
+  if (stateChanged) 
+  {
     message += "|END";
-
     // Print the message to Serial Monitor
     Serial.println(message);
-
     // Send the message over UART
     SerialPort.print(message);
-
-    // Update previous button states
-    prevButton3State = button3State;
-    prevButton5State = button5State;
-    prevButton7State = button7State;
-    prevButton36State = button36State;
-    prevButton38State = button38State;
-    prevButton40State = button40State;
   }
 }
 
 void setup() 
 {
   Serial.begin(115200);
-  
-  pinMode(BUTTON_3_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_5_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_7_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_36_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_38_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_40_PIN, INPUT_PULLUP);
-
   SerialPort.begin(115200, SERIAL_8N1, SERIAL_TX_PIN, SERIAL_RX_PIN);
+
+  // Initialize button pins as inputs with internal pull-up
+  for (int i = 0; i < numButtons; i++) 
+  {
+    pinMode(buttonPins[i], INPUT_PULLUP);
+  }
 }
 
 void loop() 
 {
   handleButtonStates(); // Function call for handling button states
-
   delay(25); // Delay to avoid excessive looping
 }
